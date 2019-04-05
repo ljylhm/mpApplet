31de2d07c5fc04182d9da3ebeb92338c344ee7e7
author <1533987417@qq.com>
2019-04-05 14:09:25 +0800

    1

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,7 @@
 //var host = 'https://www.easywork.net.cn/kjc';
 //var host = 'http://192.168.1.102:8082';
 //var host = 'http://localhost:8080'
-var host = 'http://www.wangyao.online/proseer';
+var host = 'https://www.wangyao.online/proseer';
 var api = {
 
   // 下面的地址配合云端 Demo 工作
@@ -63,7 +63,7 @@ var api = {
 
     PageNews: `${host}/PageNews`,
 
-  }
+}
 };
 
 module.exports = api;
